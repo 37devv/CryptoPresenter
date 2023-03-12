956c150e6970ccd73596cb92ae8a3950541ddc52
timojokinen <timojannejokinen@gmail.com>
2023-03-13 00:53:36 +0100

    making it work

--- CryptoPresenter.ino
+++ CryptoPresenter.ino
@@ -7,9 +7,10 @@
 #define LCD_ROWS 2
 
 LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
-StaticJsonDocument < 512 > doc;
+StaticJsonDocument<512> doc;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   lcd.init();
   lcd.backlight();
@@ -18,22 +19,32 @@ void setup() {
   lcd.print("Waiting for data");
 }
 
-void loop() {
-  if (Serial.available() > 0) {
+void loop()
+{
+
+  if (Serial.available() > 0)
+  {
+
     String jsonString = Serial.readStringUntil('\n');
+
     DeserializationError error = deserializeJson(doc, jsonString);
-    if (error) {
+    if (error)
+    {
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Error parsing JSON");
-    } else {
+    }
+    else
+    {
 
-      while (true) {
+      while (true)
+      {
+
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print("BTC/BITCOIN");
         lcd.setCursor(0, 1);
-        lcd.print(doc["bitcoin"]["chf"].as < float > (), 2);
+        lcd.print(doc["bitcoin"]["chf"].as<float>(), 2);
         lcd.print("CHF");
         delay(5000);
 
@@ -41,7 +52,7 @@ void loop() {
         lcd.setCursor(0, 0);
         lcd.print("ADA/CARDANO");
         lcd.setCursor(0, 1);
-        lcd.print(doc["cardano"]["chf"].as < float > (), 2);
+        lcd.print(doc["cardano"]["chf"].as<float>(), 2);
         lcd.print("CHF");
         delay(5000);
 
@@ -49,7 +60,7 @@ void loop() {
         lcd.setCursor(0, 0);
         lcd.print("ETH/ETHEREUM");
         lcd.setCursor(0, 1);
-        lcd.print(doc["ethereum"]["chf"].as < float > (), 2);
+        lcd.print(doc["ethereum"]["chf"].as<float>(), 2);
         lcd.print("CHF");
         delay(5000);
 
@@ -57,7 +68,7 @@ void loop() {
         lcd.setCursor(0, 0);
         lcd.print("LTC/LITECOIN");
         lcd.setCursor(0, 1);
-        lcd.print(doc["litecoin"]["chf"].as < float > (), 2);
+        lcd.print(doc["litecoin"]["chf"].as<float>(), 2);
         lcd.print("CHF");
         delay(5000);
 
@@ -65,7 +76,7 @@ void loop() {
         lcd.setCursor(0, 0);
         lcd.print("XRP/RIPPLE");
         lcd.setCursor(0, 1);
-        lcd.print(doc["ripple"]["chf"].as < float > (), 2);
+        lcd.print(doc["ripple"]["chf"].as<float>(), 2);
         lcd.print("CHF");
         delay(5000);
 
@@ -73,7 +84,7 @@ void loop() {
         lcd.setCursor(0, 0);
         lcd.print("SOL/SOLANA");
         lcd.setCursor(0, 1);
-        lcd.print(doc["solana"]["chf"].as < float > (), 2);
+        lcd.print(doc["solana"]["chf"].as<float>(), 2);
         lcd.print("CHF");
         delay(5000);
       }
--- apicall.py
+++ apicall.py
@@ -4,22 +4,20 @@ import time
 import json
 from pycoingecko import CoinGeckoAPI
 
+def makeApiCall():
+  cg = CoinGeckoAPI()
+  coingeckoresponse = cg.get_price(ids='bitcoin,litecoin,ethereum,ripple,cardano,solana', vs_currencies='chf')
+  responseToWire = json.dumps(coingeckoresponse)
+  print(responseToWire)
+  return responseToWire
+
 if __name__ == '__main__':
     #Serial communication
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
     
-    #API loop
-    print("before while true loop")
-    cg = CoinGeckoAPI()
-    coingeckoresponse = cg.get_price(ids='bitcoin,litecoin,ethereum,ripple,cardano,solana', vs_currencies='chf')
-    responseToWire = json.dumps(coingeckoresponse)
-    print(responseToWire)
-    
-    #true loop
+
     while True:
-        ser.write(responseToWire.encode())
-        print("writing")
-        #line = ser.readline().decode('utf-8').rstrip()
-        #print(line)
-        time.sleep(1)
+        res = makeApiCall()
+        ser.write((res + '\n').encode())
+        time.sleep(30)
